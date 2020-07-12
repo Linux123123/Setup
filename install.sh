889d8a2de85300088c5224448b04a64ddef1dca4
Linux123123 <linas.alexx@gmail.com>
2020-07-12 14:10:04 +0300

    Fixed Cron Job

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_URL="https://github.com/Linux1231233/setup/raw/master/scripts/"
+SCRIPT_URL="https://raw.githubusercontent.com/Linux1231233/setup/master/scripts/"
 
 if [[ $EUID -ne 0 ]]; then
   echo "* This script must be executed with root privileges (sudo)." 1>&2
@@ -27,4 +27,4 @@ echo "* Latest version is $VERSION"
 
 curl -o /opt/update.sh $SCRIPT_URL/update.sh
 
-crontab -l | { cat; echo "0 0 * * * /opt/update.sh >> /var/log/daily-update.log 2>&1"; } | crontab -
+echo '0 0 * * * root sudo bash/opt/update.sh >> /var/log/daily-update.log 2>&1' | sudo tee -a /etc/crontab
