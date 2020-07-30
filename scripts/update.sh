fbf8c26f14c2d2dda760cdfb55fc1a29bb8e8629
Linux123123 <linas.alexx@gmail.com>
2020-07-30 19:51:40 +0300

    Silence output and add noninteractive

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DEBIAN_FRONTEND=noninteractive
+
 date +"%m-%d-%y"
 date +"%T"
-apt-get update -y
-apt-get full-upgrade -y
-apt-get clean - y
-apt-get autoclean -y
+apt-get update -y -qq
+apt-get full-upgrade -y -qq
+apt-get clean - y -qq
+apt-get autoclean -y -qq
